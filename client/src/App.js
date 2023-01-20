2d05886d539a6435c3b59cd12ba5852a9f139e17
Sam Wynne <saucersam@gmail.com>
2023-01-20 11:32:40 -0600

    Creating Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
 import { useMemo } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "scenes/layout";
+import { Dashboard } from "scenes/dashboard";
 
 function App() {
   // Grabs State and passes in the theme settings to the mode in the format Material UI wants
   // https://mui.com/material-ui/customization/dark-mode/
-  const mode = useSelector((state) => state.global.mode)
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
-
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              {/* Send home page traffic to dashboard */}
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
